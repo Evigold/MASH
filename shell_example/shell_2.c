30e82a2303e73c3c187cf772c60c67b85ddc2567
evi <evigold@uw.edu>
2019-02-13 17:52:51 -0800

    makefile made

--- shell_example/shell_2.c
+++ shell_example/shell_2.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-// #include <readline/readline.h>
-#include <readline.h>
+#include <readline/readline.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
