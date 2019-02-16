bdf4e6049f74ec8ddd4f788caa256a87c3f488bd
evi <evigold@uw.edu>
2019-02-15 22:04:37 -0800

    working mash file, continuous bash script

--- src/mash.c
+++ src/mash.c
@@ -4,14 +4,58 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+char **get_command(char *cmd);
+
 int main(void) {
     size_t len = 0;
 	ssize_t read = 0;
-	char *input;
-	char **args = malloc(8*sizeof(char*));
-	const char *sep = " ";
-	char *token;
-	int i = 0; 
+	char *cmd;
+	char **args;
     int stat_loc;
     pid_t child_pid;
+
+    while (1) {
+        read = 0;
+        printf("> ");
+        read = getline(&cmd, &len, stdin);
+        args = get_command(cmd);
+
+        // Empty command
+        if (!args[0]) {
+            free(cmd);
+            free(args);
+            continue;
+        }
+        
+        // if (strcmp)
+
+        child_pid = fork();
+        // call not successful
+        if (child_pid == 0) {
+            execvp(args[0], args);
+            printf("execvp not successful\n");
+        } else {
+            waitpid(child_pid, &stat_loc, WUNTRACED);
+        }
+        free(cmd);
+        free(args);
+    }
+    return 0;
+    
+}
+
+char **get_command(char * cmd) {
+	char **args = malloc(8*sizeof(char*));
+	const char *sep = " \n";
+	char *token;
+	int i = 0; 
+
+	token = strtok(cmd, sep);
+	while (token != NULL) {
+		args[i] = strdup(token);
+		token = strtok(NULL, sep);
+		i++;
+	} 	
+	args[i] = NULL;
+	return args;
 }
